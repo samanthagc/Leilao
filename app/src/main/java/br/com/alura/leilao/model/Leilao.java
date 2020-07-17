314fa1e31992c4e15850835ab91405d3e2e09791
samanthagc <samgomescosta@gmail.com>
2020-07-17 20:31:48 -0300

    Apresenta o maior lance para cada leilão

--- app/src/main/java/br/com/alura/leilao/model/Leilao.java
+++ app/src/main/java/br/com/alura/leilao/model/Leilao.java
@@ -8,10 +8,21 @@ public class Leilao implements Serializable {
 
     private final String descricao;
     private final List<Lance> lances;
+    private double maiorLance = Double.NEGATIVE_INFINITY;
 
     public Leilao(String descricao) {
         this.descricao = descricao;
         this.lances = new ArrayList<>();
+    }
+    public void propoe(Lance lance) {
+        double valorLance = lance.getValor();
+        if (valorLance > maiorLance) {
+            maiorLance = valorLance;
+        }
+    }
+
+    public double getMaiorLance() {
+        return maiorLance;
     }
 
     public String getDescricao() {
