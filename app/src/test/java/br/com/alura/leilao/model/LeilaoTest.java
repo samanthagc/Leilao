53066b3da07c253ef1f7db1a62e9be4ae46bfa38
samanthagc <samgomescosta@gmail.com>
2020-07-18 10:53:39 -0300

    Insere classe LeilaoTest

---
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -0,0 +1,30 @@
+package br.com.alura.leilao.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LeilaoTest {
+
+    @Test
+    public void getDescricao() {
+        //criar cenário de teste
+        Leilao console = new Leilao("Console");
+
+        //executar ação experada
+        String descricaoDevolvida = console.getDescricao();
+
+        //testar resultado esperado
+        assertEquals("Console", descricaoDevolvida);
+    }
+
+    @Test
+    public void getMaiorLance() {
+        Leilao console = new Leilao("Console");
+        console.propoe(new Lance(new Usuario("Alex"), 200.0));
+
+        double maiorLanceDevolvido = console.getMaiorLance();
+
+        assertEquals(200.0, maiorLanceDevolvido, 0.0001);
+    }
+}
