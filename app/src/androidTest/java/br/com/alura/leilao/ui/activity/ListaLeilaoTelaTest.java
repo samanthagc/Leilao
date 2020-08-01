dfc2350cceeec003015b4c044e794ef488e19adb
samanthagc <samgomescosta@gmail.com>
2020-08-01 10:11:08 -0300

    Simplifica ListaLeilaoTelaTest com refatoracao

--- app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
@@ -3,12 +3,13 @@ package br.com.alura.leilao.ui.activity;
 import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
 
-import br.com.alura.leilao.api.retrofit.client.LeilaoWebClient;
 import br.com.alura.leilao.api.retrofit.client.TesteWebClient;
 import br.com.alura.leilao.model.Leilao;
 
@@ -20,27 +21,58 @@ import static org.junit.Assert.fail;
 
 public class ListaLeilaoTelaTest {
 
+    private static final String ERRO_FALHA_LIMPEZA_DE_DADOS_DA_API = "Banco de dados não foi limpo";
+    private static final String LEILAO_NAO_FOI_SALVO = "Leilão não foi salvo: ";
+
     @Rule
     public ActivityTestRule<ListaLeilaoActivity> activity =
             new ActivityTestRule<>(ListaLeilaoActivity.class, true, false);
+    private final TesteWebClient webClient = new TesteWebClient();
+
+    @Before
+    public void setup() throws IOException {
+        limpaBancoDeDadosDaApi();
+    }
 
     @Test
     public void deve_AparecerUmLeilao_QuandoCarregaUmLeilaoNaApi() throws IOException {
-        TesteWebClient webClient = new TesteWebClient();
-        boolean bancoDeDadosNaoFoiLimpo = !webClient.limpaBancoDeDados();
-
-        if (bancoDeDadosNaoFoiLimpo){
-            fail("Banco de dados não foi limpo");
-        }
-
-        Leilao tvSalva = webClient.salva(new Leilao("TV"));
-        if (tvSalva == null) {
-            fail("Leilão não foi salvo");
-        }
+        tentaSalvarLeilaoNaApi(new Leilao("TV"));
 
         activity.launchActivity(new Intent());
 
         onView(withText("TV")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void deve_AparecerDoisLeiloes_QuandoCarregarDoisLeiloesDaApi() throws IOException {
+        tentaSalvarLeilaoNaApi(new Leilao("TV"), new Leilao("Radio"));
+
+        activity.launchActivity(new Intent());
+
+        onView(withText("TV")).check(matches(isDisplayed()));
+        onView(withText("Radio")).check(matches(isDisplayed()));
+
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        limpaBancoDeDadosDaApi();
+    }
+
+    private void tentaSalvarLeilaoNaApi(Leilao... leiloes) throws IOException {
+        for (Leilao leilao : leiloes) {
+            Leilao leilaoSalvo = webClient.salva(leilao);
+            if (leilaoSalvo == null) {
+                fail(LEILAO_NAO_FOI_SALVO + leilao.getDescricao());
+            }
+        }
+    }
+
+    private void limpaBancoDeDadosDaApi() throws IOException {
+        boolean bancoDeDadosNaoFoiLimpo = !webClient.limpaBancoDeDados();
+        if (bancoDeDadosNaoFoiLimpo) {
+            fail(ERRO_FALHA_LIMPEZA_DE_DADOS_DA_API);
+        }
     }
 
 }
