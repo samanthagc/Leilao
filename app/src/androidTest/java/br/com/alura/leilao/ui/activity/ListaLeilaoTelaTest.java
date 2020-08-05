8f87b3fad99e22b6219e01e742032bc737f0c067
samanthagc <samgomescosta@gmail.com>
2020-08-04 22:38:40 -0300

    Aplica ação em novo teste

--- app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
@@ -1,6 +1,7 @@
 package br.com.alura.leilao.ui.activity;
 
 import android.content.Intent;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.After;
@@ -16,6 +17,7 @@ import br.com.alura.leilao.formatter.FormatadorDeMoeda;
 import br.com.alura.leilao.model.Leilao;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static br.com.alura.leilao.matchers.ViewMatcher.apareceLeilaoNaPosicao;
@@ -59,6 +61,27 @@ public class ListaLeilaoTelaTest {
         onView(withId(R.id.lista_leilao_recyclerview))
                 .check(matches(apareceLeilaoNaPosicao(1, "Radio", 0.00)));
 
+    }
+
+    @Test
+    public void deve_AparecerUltimoLeilao_QuandoCarregarDezLeiloesDaApi() throws IOException {
+        tentaSalvarLeilaoNaApi(
+                new Leilao("TV"),
+                new Leilao("Radio"),
+                new Leilao("Telefone"),
+                new Leilao("Smartphone"),
+                new Leilao("Notebook"),
+                new Leilao("Monitor"),
+                new Leilao("Headset"),
+                new Leilao("Pendrive"),
+                new Leilao("CPU"),
+                new Leilao("Teclado"));
+
+        activity.launchActivity(new Intent());
+
+        onView(withId(R.id.lista_leilao_recyclerview))
+                .perform(RecyclerViewActions.scrollToPosition(9))
+                .check(matches(apareceLeilaoNaPosicao(9, "Teclado", 0.00)));
     }
 
     @After
